7dc6354b01be51486e06c585b39a3095f11efce0
gy2108 <gopalyadav2108@gmail.com>
2019-06-05 20:54:21 +0530

    calculation partially implemented

--- Investment_Project/invest/views.py
+++ Investment_Project/invest/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from .forms import InvestmentForm
+import requests
+from datetime import datetime
 
-# def investmentView(request, template_name='form.html'):
-#     return render(request, template_name)
 
 class InvestmentView(TemplateView):
     template_name='form.html'
@@ -14,11 +14,29 @@ class InvestmentView(TemplateView):
 
     def post(self, request):
         if request.method == 'POST':
-            print(request.POST)
             form = InvestmentForm(request.POST)
             if form.is_valid():
                 investor_amount = form.cleaned_data['investor_amount']
                 investment_date = form.cleaned_data['investment_date']
-
-                context={'form':form,'amount':investor_amount}
+                date_today = datetime.today().strftime('%d-%b-%Y')
+                
+                print(date_today)
+                response = requests.get('http://portal.amfiindia.com/DownloadNAVHistoryReport_Po.aspx?mf=53&tp=1&frmdt=01-Apr-2015&todt='+date_today)
+                if response.status_code==200:
+                    with open('NAV_VALUES.txt', 'w+', encoding="utf-8") as f:
+                        f.write(response.text.strip())
+                else:
+                    pass
+                amount = self.calculate_investment(investor_amount, investment_date, date_today)
+                context={'form':form,'amount':amount}
                 return render(request, self.template_name, context)
+
+    def calculate_investment(self, investor_amount, investment_date, date_today):
+        with open('NAV_VALUES.txt','r') as f:
+            line = f.readline()
+            elements = line.split(';')
+            if len(elements)==8 AND elements[1]=='Axis Long Term Equity Fund - Direct Plan - Growth Option':
+                if elements[-1]==investment_date:
+                    prev_nav_value = elements[-4]
+                if elements[-1]==date_today:
+                    today_nav_value = elements[]
