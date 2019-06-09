1d1099b174da1348a489b4ba4c927f6c8f043844
gy2108 <gopalyadav2108@gmail.com>
2019-06-10 04:58:23 +0530

    form changes

--- Investment_Project/invest/forms.py
+++ Investment_Project/invest/forms.py
@@ -4,6 +4,6 @@ from django.contrib.admin.widgets import AdminDateWidget
 
 
 class InvestmentForm(forms.Form):
-    investor_amount = forms.IntegerField(help_text="Enter the Amount to invest", min_value=1, required=True)
-    investment_date = forms.DateField(widget=forms.SelectDateWidget(), initial=datetime.date.today,
-                        help_text="Enter a Date In 'YYYY-MM-DD' format after '2016-04-01'",required=True) 
+    investor_amount = forms.IntegerField(label="Investment Amount", min_value=1, required=True)
+    investment_date = forms.DateField(widget=forms.SelectDateWidget(), label="Investment Date",
+                        help_text="Enter a Date after 1st April 2016 and on or Before 6th Jun 2019",required=True) 
